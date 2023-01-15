f42469cf859ecace32e2459d0ce4e14f4dd1966c
SZ-Aarica <112103155+SZ-Aarica@users.noreply.github.com>
2023-01-15 20:49:46 +0330

    react event
    getting data from inpute

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Weather.css";
-import sunlight from "./sunlight.svg";
+
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+
 import WeatherInfo from "./WeatherInfo";
 import { MagnifyingGlass } from "react-loader-spinner";
 
-export default function Weather() {
+export default function Weather(props) {
   const [data, setData] = useState({ set: false });
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState(props.city);
   function handleSubmit(event) {
     event.oreventDefault();
+    search();
   }
   function updateCity(event) {
     setCity(event.target.value);
   }
+
   function displayDescription(response) {
     setData({
       set: true,
@@ -23,11 +25,16 @@ export default function Weather() {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
     });
   }
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
-  axios.get(url).then(displayDescription);
+
+  function search() {
+    const apiKey = "cd013e8d2828a91054e6ea3c9fd85485";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayDescription);
+  }
 
   if (data.set) {
     return (
@@ -42,40 +49,12 @@ export default function Weather() {
             />
             <input type="serach" placeholder="search a city" className="p-2" />
           </form>
-          <WeatherInfo />
-          <div className="row mt-4">
-            <div className="col-4 ms-1 text-center">
-              {city}
-              <div className="temp-data mt-3">
-                Tempature:{Math.round(data.temp)} °C
-                <br />
-                <FormattedDate date={data.date} />
-                <br />
-                date
-                <br />
-                <br />
-                {data.description}
-                <br />
-                humidity:{data.humidity}
-                <br />
-                wind:{data.wind}
-              </div>
-            </div>
-            <div className="col-4">
-              <button className="mb-4 p-1">current location</button>
-              <div className="pic mb-3 ">
-                {" "}
-                <img className="img-fluid" src={sunlight} alt="the sunlight" />
-              </div>
-            </div>
-            <div className="col-3 border border-primary mb-3 text-center">
-              forcast
-            </div>
-          </div>
+          <WeatherInfo data={data} city={city} />
         </div>
       </div>
     );
   } else {
+    search();
     return (
       <MagnifyingGlass
         visible={true}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,39 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcone from "./WeatherIcon";
 
-
-export default function WeatherInfo(){
-    return "hi";
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row mt-4">
+        <div className="col-4 ms-1 text-center">
+          {props.city}
+          <div className="temp-data mt-3">
+            Tempature:{Math.round(props.data.temp)} °C
+            <br />
+            <FormattedDate date={props.data.date} />
+            <br />
+            date
+            <br />
+            <br />
+            {props.data.description}
+            <br />
+            humidity:{props.data.humidity}
+            <br />
+            wind:{props.data.wind}
+          </div>
+        </div>
+        <div className="col-4">
+          <button className="mb-4 p-1">current location</button>
+          <div className="pic mb-3 ">
+            {" "}
+         <WeatherIcone code={props.data.icon} />
+          </div>
+        </div>
+        <div className="col-3 border border-primary mb-3 text-center">
+          forcast
+        </div>
+      </div>
+    </div>
+  );
 }
